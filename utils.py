26751ceee8066776d1d9812711511316c7c15923
osei <williamsabodunrin@gmail.com>
2024-05-11 14:37:10 +0100

    activity totals modification

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import string
 import random
 from models.question import Question
 from models import db
+from datetime import datetime
 
 def generate_code(size=6, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
+def convert_date_to_string(dt, dt_format):
+    """converts datetime to string using 'month year' format"""
+    return datetime.strptime(dt, dt_format).strftime('%b %y')
+
