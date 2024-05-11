b890cac4623d834dbef1cc03ff443979d6153a94
osei <williamsabodunrin@gmail.com>
2024-05-11 19:28:19 +0100

    handling returning activities totals

--- api/v1/views/totalsapi.py
+++ api/v1/views/totalsapi.py
@@ -5,8 +5,6 @@ from flask import jsonify
 from datetime import datetime
 from utils import convert_date_to_string
 
-date_format = "%Y-%m-%d %H:%M:%S.%f"
-
 @app_views.route('activities_total/<teacher_id>')
 def get_totals(teacher_id):
     """gets the totals for a teacher based on the month and year"""
@@ -16,9 +14,9 @@ def get_totals(teacher_id):
     quizs = teacher.quizs
     questions = teacher.questions
 
-    student_months = set([convert_date_to_string(data.created_at, date_format) for data in students])
-    quizs_months = set([convert_date_to_string(data1.created_at, date_format) for data1 in quizs])
-    questions_months = set([convert_date_to_string(data2.created_at, date_format) for data2 in questions])
+    student_months = set([convert_date_to_string(data.created_at) for data in students])
+    quizs_months = set([convert_date_to_string(data1.created_at) for data1 in quizs])
+    questions_months = set([convert_date_to_string(data2.created_at) for data2 in questions])
 
     month_year_array = student_months.union(quizs_months).union(questions_months)
     if len(month_year_array) > 12:
@@ -26,7 +24,7 @@ def get_totals(teacher_id):
     else:
         month_year_array = list(month_year_array)
 
-    month_year_array.sort(key=lambda x: datetime.strptime(x, '%b %y'))
+    month_year_array.sort(key= lambda x: datetime.strptime(x, '%b %y'))
     
     return_list = []
     for month_year in month_year_array:
@@ -35,22 +33,19 @@ def get_totals(teacher_id):
         year = month_year.split(' ')[1]
         student_list = []
         for student in students:
-            time_created = datetime.strptime(student.created_at, date_format)
-            if time_created.strftime('%b') == month and time_created.strftime('%y') == year:
+            if student.created_at.strftime('%b') == month and student.created_at.strftime('%y') == year:
                 student_list.append(student)
         quiz_list = []
         for quiz in quizs:
-            time_created = datetime.strptime(quiz.created_at, date_format)
-            if time_created.strftime('%b') == month and time_created.strftime('%y') == year:
+            if quiz.created_at.strftime('%b') == month and quiz.created_at.strftime('%y') == year:
                 quiz_list.append(quiz)
 
         question_list = []
         for question in questions:
-            time_created = datetime.strptime(question.created_at, date_format)
-            if time_created.strftime('%b') == month and time_created.strftime('%y') == year:
+            if question.created_at.strftime('%b') == month and question.created_at.strftime('%y') == year:
                 question_list.append(question)
 
-        month_year_array['month'] = month_year
+        month_total_info['month'] = month_year
         month_total_info['totalStudents'] = len(student_list)
         month_total_info['totalQuizzes'] = len(quiz_list)
         month_total_info['totalQuestions'] = len(question_list)
--- utils.py
+++ utils.py
@@ -1,13 +1,10 @@
 import string
 import random
-from models.question import Question
-from models import db
-from datetime import datetime
 
 def generate_code(size=6, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def convert_date_to_string(dt, dt_format):
+def convert_date_to_string(dt):
     """converts datetime to string using 'month year' format"""
-    return datetime.strptime(dt, dt_format).strftime('%b %y')
+    return dt.strftime('%b %y')
 
